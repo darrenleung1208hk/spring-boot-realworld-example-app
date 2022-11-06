14a703c6fd3f22a9fa72f738e376fd6237f3b387
darrenleung1208hk <leungyatlong1208@gmail.com>
2022-11-06 18:31:48 +0800

    Spring source files

---
+++ src/test/java/io/darren/realworld/example/app/RealworldExampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.darren.realworld.example.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldExampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
